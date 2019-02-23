6129680472d852cde73383969c001b0c3f4677df
RoshHoul <kabadzhg@uni.coventry.ac.uk>
2019-02-23 00:56:04 +0000

    Initial commit, player controller

---
+++ This will pass too/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    [SerializeField]
+    private float runSpeed = 40f;
+    private float horizontalMove = 0f;
+    private bool jump = false;
+    private bool crouch = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = this.GetComponent<CharacterController2D>();
+    }
+
+    // Update for taking input 
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButton("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+
+
+    }
+
+    //FU for applying the movement
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
